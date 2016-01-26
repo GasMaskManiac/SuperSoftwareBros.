69ade1678fe74eadeb6a68f1acd1fd843db6bae3
CharlieHutchinsonTheMagicalRabbit <charliehutchinson85@googlemail.com>
2016-01-26 11:23:42 +0000

    Init

---
+++ Three Amigos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Three_Amigos.MvcApplication" Language="C#" %>
---
+++ Three Amigos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Three_Amigos.Startup))]
+namespace Three_Amigos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
